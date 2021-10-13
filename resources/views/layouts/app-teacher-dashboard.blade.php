169de0417f3944272f3e37a22d14dd25dd16833d
andolino-dev <youremailsa@pgiglobal.trade>
2021-10-13 18:34:58 +0800

    remove jquery cdn

--- resources/views/layouts/app-teacher-dashboard.blade.php
+++ resources/views/layouts/app-teacher-dashboard.blade.php
@@ -161,7 +161,7 @@
 
 {{-- custom scrollbar --}}
 <!-- Google CDN jQuery with fallback to local -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+{{-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> --}}
 {{-- <script>window.jQuery || document.write('<script src="../js/minified/jquery-1.11.0.min.js"><\/script>')</script> --}}
 
 <!-- custom scrollbar plugin -->
