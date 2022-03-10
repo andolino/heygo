3fc2091052a474a60e1a3f8d844cc9ca3c42eb3a
andolino-dev <youremailsa@pgiglobal.trade>
2022-03-10 23:54:41 +0800

    updates

--- resources/js/Display/Modal/AccountSettings/ForgotPasswordModal.vue
+++ resources/js/Display/Modal/AccountSettings/ForgotPasswordModal.vue
@@ -2,23 +2,30 @@
     <div>
         <!-- @close="close" -->
         <modal :show="show" >
-            <b-form-group
-                label-cols-lg="3"
-                label="Shipping Address"
-                label-size="lg"
-                label-class="font-weight-bold pt-0"
-                class="mb-0"
-                >
-                <b-form-group
-                    label="Street:"
-                    label-for="nested-street"
-                    label-cols-sm="3"
-                    label-align-sm="right"
-                >
-                    <b-form-input id="nested-street"></b-form-input>
-                </b-form-group>
-            </b-form-group>
-            <button @click="close">Close</button>
+            <div class="row">
+                <div class="col-lg-12">
+                    <h5 class="">Hello from modal!</h5>
+                    <form autocomplete="off">
+                        <div class="d-none">
+                            <input type="password"/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="current_password" class="form-label font-12">Current Password</label>
+                            <input type="password" autocomplete="off" value="" name="current_password" class="form-control" id="current_password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="newpassword" class="form-label font-12">New Password</label>
+                            <input type="password" name="newpassword" class="form-control" id="newpassword">
+                        </div>
+                        <div class="mb-3">
+                            <label for="re_typ_newpassword" class="form-label font-12">Re-Type New Password</label>
+                            <input type="password" name="re_typ_newpassword" class="form-control" id="re_typ_newpassword">
+                        </div>
+                    </form>
+                    <button type="button" class="btn btn-warning float-right font-12 ml-1">Save</button>
+                    <button type="button" @click="close" class="btn btn-warning float-right font-12">Close</button>
+                </div>
+            </div>
         </modal>
     </div>
 </template>
@@ -35,3 +42,7 @@
         }
     }
 </script>
+
+<style>
+
+</style>
