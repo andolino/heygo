06d411a3c9751f150aa303d07d061977211c5dbc
andolino-dev <youremailsa@pgiglobal.trade>
2022-04-15 11:03:17 +0800

    updates

--- resources/js/Display/Content/TeacherProfileSettings.vue
+++ resources/js/Display/Content/TeacherProfileSettings.vue
@@ -417,6 +417,7 @@
         this.getLessonPlan();
         this.getTeachersData();
         this.getCurrencyRate();
+        alert('test!');
         // var elem = document.querySelector('input[type="range"]');
         // var rangeValue = function(){
         //   var newValue = elem.value;
